14050111286f2e50be0cc7fee53b72d8ae495015
Dave Braford-Grimes <dbraford@gmail.com>
2018-01-10 20:49:15 -0800

    renamed zxuezcontroldialog.h to xuezcontroldialog.h

--- src/qt/privacydialog.cpp
+++ src/qt/privacydialog.cpp
@@ -14,7 +14,7 @@
 #include "sendcoinsentry.h"
 #include "walletmodel.h"
 #include "coincontrol.h"
-#include "zxuezcontroldialog.h"
+#include "xuezcontroldialog.h"
 #include "spork.h"
 
 #include <QClipboard>
